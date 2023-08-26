00bdbefddb6fe609145ec9a7a9018ab9d795e147
ZhengZou2315 <zheng.zou3459@gmail.com>
2023-08-26 10:01:28 -0700

    receive snapshot

--- examples/snapshot.py
+++ examples/snapshot.py
@@ -3,7 +3,7 @@ import picamera
 def take_photo():
     with picamera.PiCamera() as camera:
         # do something with the camera
-        camera.capture('test.jpg')
+        camera.capture('test10.jpg')
 
 if __name__=='__main__':
     take_photo()
