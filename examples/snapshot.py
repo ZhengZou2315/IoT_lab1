7a102d0c3afd89bd5696793c013c2da368930ef0
zhengzou <zheng.zou3459@gmail.com>
2023-08-30 20:47:41 -0700

    update

--- examples/snapshot.py
+++ examples/snapshot.py
@@ -288,7 +288,7 @@ if __name__ == "__main__":
     # route = get_path(100, 0, cur_map, 20, 80)
     # print('route:\n',route)
 
-    move_x(10, 20, 'E')
+    move_x(10, 20, 'N')
    
   finally: 
       fc.stop()
