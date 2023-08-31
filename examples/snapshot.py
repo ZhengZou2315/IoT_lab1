4be05b5da51d60e149a511d0697cfa81bfc7083c
zhengzou <zheng.zou3459@gmail.com>
2023-08-30 18:59:43 -0700

    update

--- examples/snapshot.py
+++ examples/snapshot.py
@@ -268,7 +268,7 @@ if __name__ == "__main__":
   try: 
       # main()
     # it is reasonable to move 3cm as a step, 0.12 second.
-    move_forward(80) 
+    move_forward(120) 
     # speed_val = 25
     # for speed_val in [10, 15, 20, 25, 30, 35, 40, 45, 50]:
     #   speed4 = fc.Speed(speed_val)
