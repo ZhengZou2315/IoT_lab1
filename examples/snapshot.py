2366f840299ef80c3c3bcdc2eb36214f82399c20
zhengzou <zheng.zou3459@gmail.com>
2023-08-31 18:06:02 -0700

    update

--- examples/snapshot.py
+++ examples/snapshot.py
@@ -4,12 +4,120 @@ import numpy as np
 from collections import defaultdict
 import time, math
 
+"""Main script to run the object detection routine."""
+import argparse
+import sys
+import time
+
+import cv2
+from tflite_support.task import core
+from tflite_support.task import processor
+from tflite_support.task import vision
+import utils
+
+
+def take_snapshot() -> None:
+  """Continuously run inference on images acquired from the camera.
+
+  Args:
+    model: Name of the TFLite object detection model.
+    camera_id: The camera id to be passed to OpenCV.
+    width: The width of the frame captured from the camera.
+    height: The height of the frame captured from the camera.
+    num_threads: The number of CPU threads to run the model.
+    enable_edgetpu: True/False whether the model is a EdgeTPU model.
+  """
+
+  camera_id = 0
+  width = 640
+  height = 480
+
+  model = 'efficientdet_lite0.tflite'
+  enable_edgetpu = False
+  num_threads = 4
+
+  # Variables to calculate FPS
+  counter, fps = 0, 0
+  start_time = time.time()
+
+  # Start capturing video input from the camera
+  cap = cv2.VideoCapture(camera_id)
+  cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+  cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+  # Visualization parameters
+  row_size = 20  # pixels
+  left_margin = 24  # pixels
+  text_color = (0, 0, 255)  # red
+  font_size = 1
+  font_thickness = 1
+  fps_avg_frame_count = 10
+
+  # Initialize the object detection model
+  base_options = core.BaseOptions(
+      file_name=model, use_coral=enable_edgetpu, num_threads=num_threads)
+  detection_options = processor.DetectionOptions(
+      max_results=3, score_threshold=0.3)
+  options = vision.ObjectDetectorOptions(
+      base_options=base_options, detection_options=detection_options)
+  detector = vision.ObjectDetector.create_from_options(options)
+
+  # Continuously capture images from the camera and run inference
+  while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+      sys.exit(
+          'ERROR: Unable to read from webcam. Please verify your webcam settings.'
+      )
+
+    counter += 1
+    print('count: ',counter)
+    image = cv2.flip(image, 1)
+    print('image:\n',image)
+
+    print(' ')
+    # Convert the image from BGR to RGB as required by the TFLite model.
+    rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    print('rgb_image:\n',rgb_image)
+    print(' ')
+    # Create a TensorImage object from the RGB image.
+    input_tensor = vision.TensorImage.create_from_array(rgb_image)
+    print('input_tensor:\n',input_tensor)
+    print(' ')
+    # Run object detection estimation using the model.
+    detection_result = detector.detect(input_tensor)
+    print('detection_result:\n',detection_result)
+    print(' ')
+    # Draw keypoints and edges on input image
+    image = utils.visualize(image, detection_result)
+    print('image:\n',image)
+    print(' ')
+    # Calculate the FPS
+    if counter % fps_avg_frame_count == 0:
+      end_time = time.time()
+      fps = fps_avg_frame_count / (end_time - start_time)
+      start_time = time.time()
+
+    # Show the FPS
+    fps_text = 'FPS = {:.1f}'.format(fps)
+    text_location = (left_margin, row_size)
+    cv2.putText(image, fps_text, text_location, cv2.FONT_HERSHEY_PLAIN,
+                font_size, text_color, font_thickness)
+
+    # Stop the program if the ESC key is pressed.
+    if cv2.waitKey(1) == 27:
+      break
+    cv2.imshow('object_detector', image)
+
+  cap.release()
+  cv2.destroyAllWindows()
+
 speed = 30
 
-def take_photo():
-  with picamera.PiCamera() as camera:
-      # do something with the camera
-      camera.capture('test10.jpg')
+# def take_photo():
+  # with picamera.PiCamera() as camera:
+  #     # do something with the camera
+  #     camera.capture('test10.jpg')
 
 
 def turn_left():
@@ -346,7 +454,8 @@ def main():
 
 if __name__ == "__main__":
   try: 
-    main()
+    # main()
+    take_snapshot()
     # move_forward(10) 
     # turn_left()
     cur_map = np.zeros((200, 200))
